a48426a82ae80f0e094bf49e1b82d363be5bdbe3
Abhishek Kumar <abhishek18f@iitg.ac.in>
2021-09-12 01:57:15 +0530

    Added react frontend homepage

---
+++ react_webapp/webapp/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+// import ImageUpload from "./components/ImageUpload.jsx";
+import Title from './components/Title';
+import UploadForm from './components/UploadForm';
+ 
+function App() {
+  return (
+    <div className="App">
+      {/* <ImageUpload/> */}
+      <Title/>
+      <UploadForm/>
+    </div>
+  );
+}
+
+export default App;
